e73e09f233950be445c91427746aee0b307d7e97
gabriels3t <gabriels3t@hotmail.com>
2021-09-03 19:39:44 -0300

    Tudo funcionando

--- aula001/GeradorPrimario.cpp
+++ aula001/GeradorPrimario.cpp
@@ -4,7 +4,6 @@ void GeradorPrimario::GeneratePrimaries(G4Event* anEvent) {
     auto *particleGun = new G4ParticleGun(G4Electron::Definition()); // Criando o produtor de particulas para jogar eletrons
     particleGun->GeneratePrimaryVertex(anEvent);
     /*std::cout << "Run Id             : " << anEvent->GetEventID()<< std::endl;*/
-
     std::cout << "Particula primaria : " << particleGun->GetParticleDefinition()->GetParticleName()<< std::endl;
     std::cout << "Energia (Mev)      : " << particleGun->GetParticleEnergy()<< std::endl;
 
