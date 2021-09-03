435aee65c139f80925c1dd3024f57ce165f10694
gabriels3t <gabriels3t@hotmail.com>
2021-09-03 15:17:23 -0300

    first commit

---
+++ aula001/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "G4RunManager.hh"
+
+
+int main(){
+    // Criando o primeiro obj
+    //G4RunManager manager();
+    G4RunManager *manager = new G4RunManager();
+    
+}
