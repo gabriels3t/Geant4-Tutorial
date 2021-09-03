1544764cadce8a06679ee0a9baa8cd9c395c0cf4
gabriels3t <gabriels3t@hotmail.com>
2021-09-03 19:22:10 -0300

    Bug classe primario, nao erro ao mostrar o runID

--- aula001/GeradorPrimario.cpp
+++ aula001/GeradorPrimario.cpp
@@ -3,7 +3,8 @@
 void GeradorPrimario::GeneratePrimaries(G4Event* anEvent) {
     auto *particleGun = new G4ParticleGun(G4Electron::Definition()); // Criando o produtor de particulas para jogar eletrons
     particleGun->GeneratePrimaryVertex(anEvent);
-    //std::cout << "Run Id             : " << anEvent->GetEventID()<< std::endl;
+    /*std::cout << "Run Id             : " << anEvent->GetEventID()<< std::endl;*/
+
     std::cout << "Particula primaria : " << particleGun->GetParticleDefinition()->GetParticleName()<< std::endl;
     std::cout << "Energia (Mev)      : " << particleGun->GetParticleEnergy()<< std::endl;
 
