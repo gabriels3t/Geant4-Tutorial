b26a1d4e7f658691aaa414786c10af5662adf284
gabriels3t <gabriels3t@hotmail.com>
2021-09-04 14:17:47 -0300

    Adicionando a UI por comandos .mac

--- aula002/main.cpp
+++ aula002/main.cpp
@@ -6,27 +6,29 @@
 #include "G4VUserPhysicsList.hh"// incluindo a lista de fisica
 #include "G4PhysListFactory.hh"// incluindo para poder usar lista de fisicas ja existentes no site do cern
 #include "G4UIExecutive.hh" // incluindo a interface de usuario
-
-
+#include"G4UImanager.hh" //incluindo para poder usar comandos pre selecionados no programa
 
 int main(int argc,char** argv){
     //G4RunManager *manager = new G4RunManager();
-    auto *manager = new G4RunManager(); // auto faz com que o c++ entenda autimaticamente qual é o obj do ponteiro
+    auto *runManager = new G4RunManager(); // auto faz com que o c++ entenda autimaticamente qual é o obj do ponteiro
     auto *factory = new G4PhysListFactory(); // a fabrica da lista
     auto *listadefisica = factory->GetReferencePhysList("Shielding"); // Usando a list ja existente 
 
 
-    manager->SetUserInitialization(new MeuDetector()); // inicializador o detector 
-    manager->SetUserInitialization(listadefisica);  // inicializador a lista de fisica
-    manager->SetUserInitialization(new FonteDeParticulas());// inicializador a fonte de particulas
-    manager->Initialize();// Inicializando a simulação
+    runManager->SetUserInitialization(new MeuDetector()); // inicializador o detector 
+    runManager->SetUserInitialization(listadefisica);  // inicializador a lista de fisica
+    runManager->SetUserInitialization(new FonteDeParticulas());// inicializador a fonte de particulas
+    runManager->Initialize();// Inicializando a simulação
     
-    auto *executarUI= new G4UIExecutive(argc,argv);
-    executarUI->SessionStart();
-   // manager->BeamOn(2); // ligando o feixe de particulas (quantas particulas eu quero)
-    delete manager;
+    //auto *executarUI= new G4UIExecutive(argc,argv);
+    //executarUI->SessionStart();
+   
+    auto *uiManager = G4UImanager::GetUIpointer();
+    //uiManager->ApplyCommand("/run/beamOn 10"); // Passando o comando 
+    uiManager->ApplyCommand("/control/execute "+ G4String(argv[1])); // passando para ler um arquivo macro
+    delete runManager;
     delete factory;
-    delete executarUI;
+    //delete executarUI;
 
 
 }
