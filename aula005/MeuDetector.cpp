a0de1c9413094071e1a2452b2f3f0d625e0c6b85
gabriels3t <gabriels3t@hotmail.com>
2021-09-06 16:20:13 -0300

    Arrumando o workbench para a aula 005

---
+++ aula005/MeuDetector.cpp
@@ -0,0 +1,18 @@
+#include "MeuDetector.hpp"
+
+#include "G4Box.hh"// incluindo uma caixa
+#include "G4Material.hh" // Incluindo material
+#include "G4NistManager.hh" // Incluindo lista de material
+#include "G4LogicalVolume.hh"// Incluindo a união entre a geomatria com o material
+#include "G4VPhysicalVolume.hh" // Incluindo para um volume fisico
+#include "G4PVPlacement.hh"// Incluindo para um volume fisico
+G4VPhysicalVolume* MeuDetector::Construct(){
+    // Criando uma material - um cubo com argonio liquido
+    auto *lAr = G4NistManager::Instance()->FindOrBuildMaterial("G4_lAr"); // criando o argonio liquido
+    auto *worldBox = new G4Box("mundo",10000,10000,10000);// criando a caixa, unidade padrao mm 
+    auto *logicalWord = new G4LogicalVolume(worldBox,lAr,"Mundo Logico");// unindo no mundo logico
+    auto *mundoFisico = new G4PVPlacement(0,{0,0,0},logicalWord,"Mundo fisico",0,false,0);// volume fisico
+
+    return mundoFisico;
+    
+}
