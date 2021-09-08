45803a42f89cc86a9909d41fd6d10622426097bf
gabriels3t <gabriels3t@hotmail.com>
2021-09-07 21:56:33 -0300

    Adicionando ar, aço inox e sistema de unidade

--- aula005/MeuDetector.cpp
+++ aula005/MeuDetector.cpp
@@ -6,10 +6,15 @@
 #include "G4LogicalVolume.hh"// Incluindo a união entre a geomatria com o material
 #include "G4VPhysicalVolume.hh" // Incluindo para um volume fisico
 #include "G4PVPlacement.hh"// Incluindo para um volume fisico
+#include "SystemOfUnits.h" // Incluindo sistema de unidade 
+
 G4VPhysicalVolume* MeuDetector::Construct(){
-    // Criando uma material - um cubo com argonio liquido
-    auto *lAr = G4NistManager::Instance()->FindOrBuildMaterial("G4_lAr"); // criando o argonio liquido G4_lAr
-    auto *worldBox = new G4Box("mundo",10000,10000,10000);// criando a caixa, unidade padrao mm 
+    //CLHEP:: Namcespace para poder usar o SystemOfUnits
+
+    auto lAr = G4NistManager::Instance()->FindOrBuildMaterial("G4_lAr"); // criando o argonio liquido
+    auto ar = G4NistManager::Instance()->FindOrBuildMaterial("G4_AIR"): // criando ar
+    auto achoInox = G4NistManager::Instance()->FindOrBuildMaterial("G4_STAINLESS-STEEL"): // criando aço inox
+    auto *worldBox = new G4Box("mundo",1*CLHEP::m,1*CLHEP::m,1*CLHEP::m);
     auto *logicalWord = new G4LogicalVolume(worldBox,lAr,"Mundo Logico");// unindo no mundo logico
     auto *mundoFisico = new G4PVPlacement(0,{0,0,0},logicalWord,"Mundo fisico",0,false,0);// volume fisico
 
